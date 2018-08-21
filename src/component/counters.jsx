8b741eef036ee73387bded2a8c27efd980edd342
Viet Ma <vietma@Viets-MacBook-Pro.local>
2018-08-21 16:41:03 +1000

    added Reset button

--- src/component/counters.jsx
+++ src/component/counters.jsx
@@ -11,18 +11,43 @@ class Counters extends Component {
     ]
   };
 
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    /* counters[0].value++;
+    console.log(counters[0]); */
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
   handleDelete = counterId => {
     console.log("Event Handler Called", counterId);
     const counters = this.state.counters.filter(c => c.id !== counterId);
-    this.setState({counters: counters});
+    this.setState({ counters: counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
   };
 
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map(counter => (
           <Counter
             onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
             key={counter.id}
             counter={counter}
           />
